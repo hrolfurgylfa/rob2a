f0855cc79f6b5ec10328c48f14af4fb0398d2d13
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-09-12 11:07:59 +0000

    fall inni í skjali

--- includes/headers/rob2a_header.h
+++ includes/headers/rob2a_header.h
@@ -1,7 +1,8 @@
 #ifndef rob2a_header
 #define rob2a_header
 #endif
-#define BASETIME 1700
+
+#define BASETIME_METER 3400
 #define FULLPOWER 127
 
 void drive_time(float distance, bool forward, int BASETIME);
--- verkefni2/suiside/suiside.c
+++ verkefni2/suiside/suiside.c
@@ -13,21 +13,25 @@
 #pragma config(Motor,  port7,           arm_motor,     tmotorServoContinuousRotation, openLoop, reversed)
 #pragma config(Motor,  port10,          right_motor,   tmotorVex269, openLoop, encoder, encoderPort, dgtl7, 1000)
 
+//#include "../../includes/headers/rob2a_header.h"
+//#include "../../includes/functions/motorFunctions.inc"
 
-//----------------------------------------------|INCLUDE|----------------------------------------------
+#define BASETIME_METER 3400
+#define FULLPOWER 127
 
-#include "../includes/headers/rob2a_header.h"
-#include "../includes/functions/motorFunctions.inc"
+void drive_time(float distance, bool forward, int BASETIME_METER){
 
-//+++++++++++++++++++++++++++++++++++++++++++++| MAIN |+++++++++++++++++++++++++++++++++++++++++++++++
+	int speed = (forward)? (127):(-127);
+	motor[right_motor] = speed;
+	motor[left_motor]  = speed;
+	wait1Msec(distance * BASETIME_METER);
+}
+
 task main(){
 
 	wait1Msec(2000);
-	for(float distance = 0.5; i <=2.5;distance+=0.5){
-		drive_time(distance,true,BASETIME);
-		drive_time(distance,false,BASETIME);
+	for(float distance = 0.5; distance <= 2.5; distance += 0.5){
+		drive_time(distance, true, BASETIME_METER);
+		drive_time(distance, false, BASETIME_METER);
 	}
-
-
 }
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
