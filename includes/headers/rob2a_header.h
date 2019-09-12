e5e481d9b961e4596c79d311ab9a83c857ce7059
ivarthor <ivarthor01@gmail.com>
2019-09-12 12:07:03 +0000

    kommentaði

--- includes/headers/rob2a_header.h
+++ includes/headers/rob2a_header.h
@@ -2,7 +2,9 @@
 #define rob2a_header
 #endif
 
+//-----------------------------|define variables|------------------------------------------------
 const int BASETIME_METER = 3400;
 const int FULLPOWER = 127;
 
+//---------------------------|initialize functions|--------------------------------------------
 void drive_time(float distance, bool forward, int BASETIME_METER);
