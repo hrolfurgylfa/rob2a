c42e4659578e0c18a41c3f0f40d1c653fbe92849
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-10-01 09:29:44 +0000

    BASE TURN bætt við

--- includes/headers/rob2a_header.h
+++ includes/headers/rob2a_header.h
@@ -6,10 +6,11 @@
 const int BASETIME_METER = 3400;
 const int FULLPOWER = 127;
 const int BASE_DEGREES_FOR_METER = 1080;
+const float BASE_TURN = 3;
 
 //---------------------------|initialize functions|--------------------------------------------
 
 void drive_time(float distance, bool forward, int BASETIME_METER);
 void drive(float dist, bool forward, int BASE_DIST);
-void turn(int deg, bool r_l);
+void turn(int deg, bool r_l, float BASE_TURN);
 void drive_turn(int* drive_turn_list, int BASE_DEGREES_FOR_METER);
