0be0538a8818dd36b3dac36c6b67de3234c58f6a
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-10-08 10:01:22 +0000

    verkefni 2 föll commentuð og drive_turn lagað

--- includes/functions/motorFunctions.c
+++ includes/functions/motorFunctions.c
@@ -12,75 +12,85 @@ void drive_time(float distance, bool forward, int BASETIME_METER){
 	motor[left_motor] = 0;
 }
 
+// drive fallið lætur vélmennið keyra ákveðna fjarægð, afturábak eða áfram en er mun nákvæmara en drive_time og róbótinn begir ekki
 void drive(float dist, bool forward, int BASE_DIST){
-	SensorValue[quadrature_right] = 0;	  // Set the encoder so that it starts counting at 0
-	SensorValue[quadrature_left]  = 0;	  // Set the encoder so that it starts counting at 0
-	int speed100 =(forward)? (100):(-100);
-	int speed74 =(forward)? (74):(-74);
-	int speed127 =(forward)? (127):(-127);
+	SensorValue[quadrature_right] = 0;	  // quadrature_right skynjarinn settur á 0 svo að það sé hgt að nota það í reikningana
+	SensorValue[quadrature_left]  = 0;	  // quadrature_left skynjarinn settur á 0 svo að það sé hgt að nota það í reikningana
+	// Þetta eru mismunandi variables vegna þess að annars begir róbótinn of mikið og nær ekki að rétta sig almennilega
+	int speed100 =(forward)? (100):(-100);// Átt reiknuð fyrir 100 hraðann
+	int speed74 =(forward)? (74):(-74);// Átt reiknuð fyrir 74 hraðann
+	int speed127 =(forward)? (127):(-127);// Átt reiknuð fyrir 127 hraðann
 
-	while(dist * BASE_DIST > abs(SensorValue[quadrature_right]))		// Creates an infinite loop, since "true" always evaluates to true
-	{
+	// Þetta keyrir þangað til quadrature_right skynjarinn segir að það sé búið að keyra nógu langt
+	while(dist * BASE_DIST > abs(SensorValue[quadrature_right])) {
+
+		// Hérna geymum við quadrature outputtið í variables til þess að þurfa ekki alltaf að reikna absaloute value
 		int motor_right_value = abs(SensorValue[quadrature_right]);
 		int motor_left_value = abs(SensorValue[quadrature_left]);
 
-		if(motor_right_value == motor_left_value) // If quadrature_right has counted the same amount as quadrature_left:
-		{
-			// Move Forward
-			motor[right_motor] = speed100;		    // Right Motor is run at power level 80
-			motor[left_motor]  = speed127;		    // Left Motor is run at power level 80
+		// Þetta keyrir ef róbótinn er að keyra þráð beint
+		if(motor_right_value == motor_left_value){
+			motor[right_motor] = speed100;
+			motor[left_motor]  = speed127;
 		}
-		else if(motor_right_value > motor_left_value)	// If quadrature_right has counted more encoder counts
-		{
-			// Turn slightly right
-			motor[right_motor] = speed74;		    // Right Motor is run at power level 60
-			motor[left_motor]  = speed127;		    // Left Motor is run at power level 80
+		// Þetta keyrir ef róbótinn er að begja til vinstri
+		else if(motor_right_value > motor_left_value){
+			motor[right_motor] = speed74;
+			motor[left_motor]  = speed127;
 		}
-		else	// Only runs if quadrature_left has counted more encoder counts
-		{
-			// Turn slightly left
-			motor[right_motor] = speed127;		    // Right Motor is run at power level 80
-			motor[left_motor]  = speed127;		    // Left Motor is run at power level 60
+		// Þetta keyrir ef rúbótinn er að begja til hægri
+		else {
+			motor[right_motor] = speed127;
+			motor[left_motor]  = speed127;
 		}
 	}
 
+	// Hérna er róbótinn búinn svo að hann stoppar báða mótorana
 	motor[right_motor] = 0;
 	motor[left_motor]  = 0;
 }
 
-void drive_turn(int* drive_turn_list, int BASE_DEGREES_FOR_METER, float BASE_TURN){
-	for(int i = 0; i < 30; i = i + 2) {
-		int num_1 = drive_turn_list[i];
-		int num_2 = drive_turn_list[i+1];
+void drive_turn(int* drive_turn_list, int BASE_DEGREES_FOR_METER, float BASE_TURN, int arrey_length){
+	// Hérna nota ég variable sem var sent inn í fallið, arrey_length, vegna þess að það er ekki hægt að ná lengdini af pointer list
+	for(int i = 0; i < arrey_length; i = i + 2) {
+		int num_1 = drive_turn_list[i];// Þetta er talan hversu mikið á að keyra
+		int num_2 = drive_turn_list[i+1];// Þetta er talan hversu mikið á að begja
 		writeDebugStream("Num 1: %d ",num_1);
 		writeDebugStream("Num 2: %d ",num_2);
 		writeDebugStream("i: %d ",i);
 
-		bool forward;
+		bool forward;// Þessi bool segir hvort að róbótinn eigi að fara afturábak eða áfram, þetta er svo sent inn í fallið drive();
+		// Hérna er ákveðið hvort að forward eigi að vera true eða false eftir því hvort num_1 (fjarlægðin sem á að keyra) sé í mínus eða plús
 		if (num_1 > 0){ forward = true; }
 		else { forward = false; }
 
+		// Hérna er breytt num_1 í float og sett í num_1_float
 		float num_1_float = *(float *)&num_1;
+		// Drive distance reiknað vegna þess að það kom inn í þetta fall sem cm en þarf að vera metrar fyrir drive() fallið
 		float drive_distance = abs(num_1_float) / 100;
 		writeDebugStream("Afram: %f ",drive_distance);
 
+		// Drive fallið kallað
 		drive(drive_distance, forward, BASE_DEGREES_FOR_METER);
-		wait1Msec(1000);
+		wait1Msec(1000);// Hérna er beðið svo að róbótinn verði nákvæmari þegar hann er að keyra brautina
 
+		// Þetta keyrir bara ef snúningurinn er ekki 0
 		if (num_2 != 0) {
-			bool direction;
+			bool direction;// Þessi bool segir hvort að róbótinn eigi að begja til hægri eða vinstri, þetta er svo sent inn í fallið turn()
+			// Hérna er ákveðið hvort að direction eigi að vera true eða false eftir því hvort num_2 (snúningurinn) sé í mínus eða plús
 			if (num_2 > 0){ direction = true; }
 			else { direction = false; }
 			
 			writeDebugStream("Begja: %d \n",abs(num_2));
-			turn(abs(num_2), direction, BASE_TURN);
-			wait1Msec(1000);
+			turn(abs(num_2), direction, BASE_TURN);// Turn fallið keyrt
+			wait1Msec(1000);// Hérna er beðið svo að róbótinn verði nákvæmari þegar hann er að keyra brautina
 		}
 	}
 }
 
 void turn(int deg, bool r_l, float BASE_TURN){
 
+	// Róbótinn á að begja til hægri
 	if (r_l == true){
 
 		SensorValue[quadrature_right] = 0;
@@ -93,6 +103,7 @@ void turn(int deg, bool r_l, float BASE_TURN){
 
 		}
 	}
+	// Róbótinn á að begja til vinstri
 	else if (r_l == false){
 
 		SensorValue[quadrature_right] = 0;
@@ -106,6 +117,7 @@ void turn(int deg, bool r_l, float BASE_TURN){
 		}
 	}
 
+	// Hérna er róbótinn búinn svo að hann stoppar báða mótorana
 	motor[right_motor] = 0;
 	motor[left_motor] = 0;
 }
