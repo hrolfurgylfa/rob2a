9c42519cce80596af036b626530b290b89dd5f93
ivarthor <ivarthor01@gmail.com>
2019-10-17 11:15:36 +0000

    kommentað verkefni 3

--- verkefni3/1. hluti/1. hluti.c
+++ verkefni3/1. hluti/1. hluti.c
@@ -12,6 +12,29 @@
 #pragma config(Motor,  port6,           claw_motor,    tmotorServoContinuousRotation, openLoop)
 #pragma config(Motor,  port7,           arm_motor,     tmotorServoContinuousRotation, openLoop, reversed)
 #pragma config(Motor,  port10,          right_motor,   tmotorVex269, openLoop, encoder, encoderPort, dgtl7, 1000)
+/*----------------------------------------------------------------------------------------------------*\
+|*                                         - Fjarstýring -                                            *|
+|*                                      ROBOTC on VEX 2.0 CORTEX                                      *|
+|*                                                                                                    *|
+|*                            Gerir kleyft að stjórna vélmenninu með fjartýringu                      *|
+|*                                                                                                    *|
+|*                                                                                                    *|
+|*                                        ROBOT CONFIGURATION                                         *|
+|*    NOTES:                                                                                          *|
+|*    1)  Kalla fallið sem gerir kleyft að keyra vélmennið með fjarstýringunni                        *|
+|*        	                                                                                          *|
+|*    2)  Kalla fallið sem gerir kleyft að stýra arminum með fjarstýringunni                          *|
+|*                                                                                                    *|
+|*    3)  Kalla fallið sem gerir kleyft að stýra klónni með fjarstýringunni                           *|
+|*                                                                                                    *|                                                                                                    *|
+|*    MOTORS & SENSORS:                                                                               *|
+|*    [I/O Port]          [Name]              [Type]                [Description]                     *|
+|*    Motor Port 1        left_motor          VEX 2-wire module     Left side motor                   *|
+|*    Motor Port 6        claw_motor          VEX 3-wire module     claw motor                        *|
+|*    Motor Port 7        arm_motor           VEX 3-wire module     arm motor                         *|
+|*    Motor Port 10       right_motor         VEX 2-wire module     Right side motor                  *|            
+\*----------------------------------------------------------------------------------------------------*/
+
 
 //---------------------------------------|INCLUDES|-------------------------------------------------------------------------------------------------------
 
