4ad9e891648934bba0f4c285b4fb4820c55adfb0
ivarthor <ivarthor01@gmail.com>
2019-11-12 09:49:17 +0000

    haeder bætt við

--- includes/headers/rob2a_header.h
+++ includes/headers/rob2a_header.h
@@ -8,7 +8,7 @@ const int FULLPOWER = 127;
 const int BASE_DEGREES_FOR_METER = 1080;
 const float BASE_TURN = 3;
 const int JOY_THREASHOLD = 10;
-
+const int LINE_THREASHOLD = 0;
 //---------------------------|initialize functions|--------------------------------------------
 
 void drive_time(float distance, bool forward, int BASETIME_METER);
@@ -16,4 +16,7 @@ void drive(float dist, bool forward, int BASE_DIST);
 void drive_turn(int* drive_turn_list, int BASE_DEGREES_FOR_METER, int arrey_length);
 void turn(int deg, bool r_l, float BASE_TURN);
 void drive_controller(int left_forward, int right_forward);
+void arm_controller(int upp_btn, int down_btn);
+void claw_controller(int close_btn, int open_btn);
+void sonar_drive(int v);
 task emergency_stop();
